12a174ecdfb2353cecf1956c95c1fc0fe34e331a
admim <admin@android-4be2246982f81526.home>
2020-09-17 17:00:17 +0100

    fixed bugs

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 var path = require('path');
+var bodyParser = require('body-parser');
+var url = 'mongodb://localhost:27017/myapp';
 
-app.set('view engine', 'pug');
-app.set('views','./views');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/myapp");
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String
+});
+var User = mongoose.model("User", nameSchema);
 
 app.get('', function(req, res) {
   res.sendFile(path.join(__dirname + '/form/form.html'));
 });
 
-app.get('/views', function(req, res){
-   res.render('views');
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database")
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
 });
 
-app.use(express.urlencoded({
-  extended: true
-}))
 
-app.post('/form', (req, res) => {
-  const username = req.body.username
-  //...
-  res.end()
-})
 
-app.listen(3000, function() {
-  console.log('App listening on port 3000!');
+
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
 });
